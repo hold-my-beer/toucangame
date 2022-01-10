b1e1fd30125b16be1984ed5983760ae1bb118430
Sergey Petrov <sergey.otis80@gmail.com>
2022-01-10 20:36:40 +0300

    Deployment 1.0

--- server.js
+++ server.js
@@ -242,6 +242,16 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running at port ${PORT}`));
