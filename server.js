26baefb8987b58d3c2c505786e39bb4938d46169
Sergey Petrov <sergey.otis80@gmail.com>
2022-01-10 20:56:12 +0300

    Deployment __dirname removed

--- server.js
+++ server.js
@@ -242,7 +242,7 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
